d6422b36f11d3db0bf51b5f4e3e27776885033a2
Ryan Gross <ryangross123@icloud.com>
2019-02-20 22:22:11 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Homework_3
+{
+    class Program
+    {
+        static void LogIn()
+        {
+            string email = "";
+
+            Console.WriteLine("What is your email?");
+            email = Console.ReadLine();
+
+            using (var db = new AppDbContext())
+            {
+                User Email = db.User.Where(u => u.Email == email);
+            }
+        }
+
+
+        static void ListQuestions()
+        {
+
+        }
+
+        static void ListUnanswered()
+        {
+
+        }
+
+        static void AskQuestion()
+        {
+
+        }
+
+        static void RemoveQuestion()
+        {
+
+        }
+
+        static void Answer()
+        {
+
+        }
+
+
+        static void Main(string[] args)
+        {
+             using (var db = new AppDbContext())
+        {
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+        } 
+
+            int UserChoice;
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("Type 1 to list all questions");
+            Console.WriteLine("Type 2 to list only unanswered questions"); 
+            Console.WriteLine("Type 3 to ask a question");
+            Console.WriteLine("Type 4 to remove a question");
+            Console.WriteLine("Type 5 to answer a question");
+            
+            UserChoice = Convert.ToInt32(Console.ReadLine());
+            switch(UserChoice)
+            {
+                case 1:
+                    ListQuestions();
+                    break;
+                case 2:
+                    ListUnanswered();
+                    break;
+                case 3:
+                    AskQuestion();
+                    break;
+                case 4:
+                    RemoveQuestion();
+                    break;
+                case 5:
+                    Answer();
+                    break;
+                default:
+                    Console.WriteLine($"Unexpected value of ({UserChoice})");
+                    break;
+            }
+        }
+    }
+}
---
+++ model.cs
@@ -0,0 +1,67 @@
+/*  
+    Each user can post many questions; each question is posted by one user.
+    Each user can post many answers; each answer is posted by one user.
+    Each question can have many answers; each answer applies to one question.
+*/
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Homework_3
+{
+public class AppDbContext : DbContext
+    {
+         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = database.db");
+        }
+
+        public DbSet<User> Users {get; set;}
+        public DbSet<Question> Questions {get; set;}
+        public DbSet<Answer> Answers {get; set;}
+    }
+
+    public class User
+    {
+        public string UserID {get; set;}    // PK
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+        public string Email {get; set;}
+        public DateTime RegistrationDate {get; set;}
+        public List<Question> Questions {get; set;}
+        public List<Answer> Answers {get; set;}
+
+        public override string ToString()
+        {
+            return ($"{UserID}: {FirstName} {LastName}, {Email}, {RegistrationDate}");
+        }
+    }
+
+    public class Question
+    {
+        public int QuestionID {get; set;}   // PK
+        public int QuestionText {get; set;}
+        public DateTime QuestionDate {get; set;}
+        public string UserID {get; set;}    // FK
+        public List<Answer> Answers {get; set;}
+
+        public override string ToString()
+        {
+            return ($"{QuestionID}: {QuestionText} - {QuestionDate}");
+        }
+    }
+
+    public class Answer
+    {
+        public int AnswerID {get; set;}     // PK
+        public string AnswerText {get; set;}
+        public DateTime AnswerDate {get; set;}
+        public string UserID {get; set;}    // FK
+        public int QuestionID {get; set;}   //FK
+
+        public override string ToString()
+        {
+            return ($"{AnswerID}: {AnswerText} - {AnswerDate}");
+        }
+    }
+}
